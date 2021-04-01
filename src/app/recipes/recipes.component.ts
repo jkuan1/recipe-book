90b1ccd4a7d0c5a69ffdb4f503568b4d7befe952
jkuan1 <justinkuan2000@gmail.com>
2021-03-31 22:56:31 -0700

    added a simple recipe skeleton structure

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  
+  recipes = []; 
 
   constructor() { }
 
