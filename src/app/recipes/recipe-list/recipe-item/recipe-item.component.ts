2b0e177d8c9fcdee64331cddeedbb20f50897d63
jkuan1 <justinkuan2000@gmail.com>
2021-04-10 22:33:38 -0700

    created a responsive recipe detail component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.module';
 
 @Component({
@@ -8,8 +8,15 @@ import { Recipe } from '../../recipe.module';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelected() {
+
+    this.recipeSelected.emit()
+
+  }
 }
