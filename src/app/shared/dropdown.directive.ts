4fdefdfe97ea875a7c9bda7409581fa9442c6388
jkuan1 <justinkuan2000@gmail.com>
2021-04-13 21:41:06 -0700

    added feature that allows dropdowns to close when user clicks anywhere on the dom

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -13,10 +13,12 @@ export class DropdownDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
   @HostBinding('class.show') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click',['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.el.nativeElement.contains(event.target) ? !this.isOpen : false;
+
     let part = this.el.nativeElement.querySelector('.dropdown-menu');
-    if (this.isOpen) this.renderer.addClass(part, 'show');
+    if (this.isOpen && this.el.nativeElement.contains(event.target)) this.renderer.addClass(part, 'show');
     else this.renderer.removeClass(part, 'show')
   }
+
 }
