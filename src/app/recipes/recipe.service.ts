52a2b40871b71857691120685eee385158d2351c
jkuan1 <justinkuan2000@gmail.com>
2021-05-16 15:04:02 -0700

    added Ingredient listing prototype code

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,32 @@
 import { EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredient.model';
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'Schnitzel recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+          new Ingredient('Meat', 1),
+          new Ingredient("Fries", 20)
+      ]
+    ),
+    new Recipe(
+      'Cheese and Bacon Burger',
+      'Not for the weak of stomach',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Meat', 3),
+        new Ingredient('Cheese', 2),
+        new Ingredient('Buns', 2)
+      ]
+    ),
+  ];
 
-    recipeSelected = new EventEmitter<Recipe>();
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-      ];
-
-
-    getRecipes() {
-        return this.recipes.slice();
-    }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
