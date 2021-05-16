5674ffa984f184f7cc7e48cc74a2b503a09ff314
jkuan1 <justinkuan2000@gmail.com>
2021-05-16 14:35:32 -0700

    updated the code to use the shopping list service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,12 +1,11 @@
 import {
   Component,
   ElementRef,
-  EventEmitter,
   OnInit,
-  Output,
   ViewChild,
 } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,18 +16,14 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor(private slService: ShoppingListService) {}
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(){}
 
   onAddItem() {
-    const newIngredient = new Ingredient(
+    this.slService.onIngredientAdded(new Ingredient(
       this.nameInputRef.nativeElement.value,
       this.amountInputRef.nativeElement.value
-    );
-
-    this.ingredientAdded.emit(newIngredient);
+    ));
   }
 }
